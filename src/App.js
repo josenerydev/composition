d410f9c77ddbc43b9e6f14cca7282f8f0cf47b8d
José Nery <josenerydev@gmail.com>
2020-03-19 12:40:30 -0300

    Creating a Specialized Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { GeneralList } from './GeneralList';
+import { SortedList } from './SortedList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            names: ["Zoe", "Bob", "Alice", "Dora", "Joe"]
+        }
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-6">
+                        <GeneralList list={this.state.names} theme="primary" />
+                    </div>
+                    <div className="col-6">
+                        <SortedList list={this.state.names} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
-
-export default App;
