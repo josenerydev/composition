084732312f19eaa49a5f1888e32d9baf60af55e2
José Nery <josenerydev@gmail.com>
2020-03-23 07:16:33 -0300

    Chapter 14

--- src/ActionButton.js
+++ src/ActionButton.js
@@ -1,12 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { ProModeContext } from "./ProModeContext";
 
 export class ActionButton extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            clickCount: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState({ clickCount: this.state.clickCount + 1 });
+        this.props.callback();
+    }
+
     render() {
         return (
-            <button className={`btn btn-${this.props.theme} m-2`}
-                onClick={this.props.callback}>
-                {this.props.text}
-            </button>
+            <ProModeContext.Consumer>
+                {contextData => {
+                    if (this.state.clickCount > 1) {
+                        throw new Error("Click Counter Error");
+                    }
+                    return <button
+                        className={this.getClasses(contextData.proMode)}
+                        disabled={!contextData.proMode}
+                        onClick={this.handleClick}>
+                        {this.props.text}
+                    </button>
+                }}
+            </ProModeContext.Consumer>
         )
+    }
+
+    getClasses(proMode) {
+        let col = proMode ? this.props.theme : "danger";
+        return `btn btn-${col} m-2`;
     }
 }
